fc93d20db052f2cb73f20ef1fa7d05527c92240d
arielclarence <ariel.clarence.t@gmail.com>
2023-09-20 22:07:05 +0200

    FirstInit

---
+++ src/test/java/fontys/sem3/school/controller/CuisineControllerTest.java
@@ -0,0 +1,18 @@
+package fontys.sem3.school.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class CuisineControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAllGenre() {
+        assertTrue(true);
+    }
+}
