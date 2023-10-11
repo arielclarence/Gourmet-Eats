02cd900023ca4c698cea22202350d71aa3de8616
arielclarence <ariel.clarence.t@gmail.com>
2023-10-11 22:33:08 +0200

    CORS

--- src/main/java/fontys/sem3/school/controller/CuisinesController.java
+++ src/main/java/fontys/sem3/school/controller/CuisinesController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/cuisines")
 @AllArgsConstructor
